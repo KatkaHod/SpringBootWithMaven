a447dc0f715d09a6d01523f001cadf5439ff4d39
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-08-13 21:38:08 +0200

    Initial commit

---
+++ src/main/java/com/example/SpringBootWithMaven/SpringBootWithMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootWithMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithMavenApplication.class, args);
+	}
+
+}
