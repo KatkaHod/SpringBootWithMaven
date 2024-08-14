11135db6e60b22d5d81555a465813c615cab7fff
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-08-14 14:30:29 +0200

    PathController

--- src/main/java/com/example/SpringBootWithMaven/controller/PathController.java
+++ src/main/java/com/example/SpringBootWithMaven/controller/PathController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+import java.util.Objects;
+
 @RestController
 @RequestMapping("path")
 
@@ -23,6 +26,17 @@ public class PathController {
         }
     }
 
+    private String findCountry(String country) {
+        return switch (country) {
+            case "CZ" -> "Czech Republic";
+            case "DE" -> "Germany";
+            case "SK" -> "Slovakia";
+            case "PL" -> "Poland";
+            default -> "The country is not available";
+        };
+    }
+
+
     @GetMapping("v1/employee")
     public String getEmployeeName() {
         return findEmployeeById(1L);
@@ -34,5 +48,41 @@ public class PathController {
     public String getEmployeeById(@PathVariable Long userId) {
         return findEmployeeById(userId);
     }
+
+    @GetMapping("v3/employee/{id}")
+    public String getEmployeeByIdWithName(@PathVariable("id") String thisIsTheIdFromUser
+    ){
+        return findEmployeeById(Long.parseLong(thisIsTheIdFromUser));
+    }
+
+    //two input parameters
+    @GetMapping ("v5/employee/{id}/{country}")
+    public String getEmployeeByIdAndCountry (
+            @PathVariable(value = "id") Long userId,
+            @PathVariable(value = "country") String country
+    ){
+        String answerId = findEmployeeById(userId);
+        String answerCountry = findCountry(country);
+
+        //return answerId + " " + answerCountry;
+        return answerId.concat(" ").concat(answerCountry);
+    }
+
+    @GetMapping("v6/employee/{id}/{country}")
+    public String getEmployeeInMap(
+            @PathVariable Map<String,String> pathVariables
+
+    ) {
+        String id = pathVariables.get("id");
+        String country = pathVariables.get("country");
+        return findEmployeeById(Long.parseLong(id).concat(" ").concat(findCountry(country)));
+        //return pathVariables.toString();
+    }
+
+
+
+
+
+
 
 }
